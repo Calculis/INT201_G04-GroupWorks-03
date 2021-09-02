36bce4e1709a670f3e070df98defb7e2d771bc93
Phunon Srivoraluxkana <phunon.sriv@mail.kmutt.ac.th>
2021-09-02 19:16:18 +0700

    better version

--- Lab_3.js
+++ Lab_3.js
@@ -1,25 +1,31 @@
 class person{
-    constructor(name,setOfQ,setOfA,){
+    constructor(name,setOfQA){
         this.point=0
         this.name=name
-        this.setOfA=setOfA
-        this.setOfQ=setOfQ
+        // this.setOfA=setOfA
+        // this.setOfQ=setOfQ
+        this.setOfQA=setOfQA
     }
 }
 
 let question = [
     {title:"10+20",choice:[10,20,30],answer:30,qNumber:1},
-    {title:"A->B->?",choice:['D','G','C'],answer:'C',qNumber:2}
+    {title:"A->B->?",choice:['D','G','C'],answer:'C',qNumber:2},
+    {title:"My Favorite band",choice:['Three man down','Tilly Bird','Indigo'],answer:'Three Man Down',qNumber:3}
 ]
 
-let a=new person("ALICE",[1,2],[20,'C'])
+// let a=new person("ALICE",[1,3],[20,'Three Man Down'])
+let a =new person("Phunon",[{questionNo:3,answer:'Three Man Down'},{questionNo:1,answer:30}])
 
 function check(player,question){
-    for(let l=0;l<player.setOfQ.length;l++){
-        let matchQuestion=findQ(player.setOfQ[l])
-        if(player.setOfA[l]==matchQuestion.answer){
+    for(let l=0;l<player.setOfQA.length;l++){
+        let matchQuestion=findQ(player.setOfQA[l].questionNo)
+        if(player.setOfQA[l].answer==matchQuestion.answer){
             player.point++
-            console.log("Done1")
+            console.log(`Question Number ${player.setOfQA[l].questionNo} is corrected!`)
+        }
+        else{
+            console.log(`Question Number ${player.setOfQA[l].questionNo} is incorrected!`)
         }
     }
 }
@@ -32,3 +38,4 @@ function findQ(qNumber){
     return null
 }
 check(a,question)
+console.log(a.point)
